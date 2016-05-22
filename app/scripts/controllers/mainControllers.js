324c94d503def3cd6b663a2f2d38e0b5d88c30d5
S.Hu <ihsx@qq.com>
2016-05-22 19:52:49 +0800

    homepageTalkingsController

--- app/scripts/controllers/mainControllers.js
+++ app/scripts/controllers/mainControllers.js
@@ -49,4 +49,56 @@ PCControllers.controller('indexController', ['$scope', function ($scope) {
         }
         for (var index in $scope.persons) getUserInfo(index);
     });
+}])
+.controller('homepageTalkingsController', ['$scope', 'Talking', 'User', function ($scope, Talking, User) {
+    var lastCheckTime;
+    var currentPage = 0;
+    var pages = 0;
+
+    $scope.contents = [];
+    $scope.hasNextPage = false;
+    $scope.hasNew = false;
+    $scope.newCount = 0;
+
+    function getUserInfo(Array, index) {
+        User.query(Array[index].user_uid, function (res) {
+            Array[index].user_nickname = res.data.data.nickname;
+            Array[index].user_avatar = res.data.data.avatar;
+        });
+    }
+
+    $scope.getNextPageContents = function () {
+        ++currentPage;
+        if (currentPage == 1) lastCheckTime = new Date().toLocaleTimeString();
+
+        Talking.query((currentPage == 1) ? null : {page: currentPage}, function (response) {
+            var newRows = response.data.rows;
+            for (var index in newRows) getUserInfo(newRows, index);
+            $scope.contents = $scope.contents.concat(newRows);
+            if(response.data.pages) pages = response.data.pages;
+            $scope.hasNextPage = pages > currentPage;
+        });
+
+    };
+
+    function checkNew() {
+        Talking.queryCount({after: lastCheckTime}, function (response) {
+            $scope.newCount = response.data;
+            $scope.hasNew = $scope.newCount > 0;
+        });
+    }
+
+    $scope.getNewContents = function () {
+        Talking.query({after: lastCheckTime}, function (response) {
+            var newRows = response.data.rows;
+            for (var index in newRows) getUserInfo(newRows, index);
+            $scope.contents = newRows.concat($scope.contents);
+            checkNew();
+        });
+        lastCheckTime = new Date().toLocaleTimeString();
+    };
+
+    $scope.getNextPageContents();
+    setInterval("checkNew()", 30*1000);
+
 }]);
